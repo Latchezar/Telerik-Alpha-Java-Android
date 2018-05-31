cce1c75caf48e92ad68d352989e13e7430640fa4
Latchezar Nikolov <latcho.nikolov@gmail.com>
2018-05-31 15:20:18 +0300

    task

---
+++ Reverse Array/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String str = in.nextLine();
+        String[] arr = str.split(" ");
+        for (int i = arr.length - 1; i >= 0 ; i--) {
+            if (i == 0) {
+                System.out.print(arr[i]);
+            } else {
+                System.out.print(arr[i] + ", ");
+            }
+        }
+    }
+}
